0d528e53ae1624b8b0e366e821590b61655f7730
Cayonliow <cayon.1318.96@hotmail.com>
2016-03-07 23:44:47 +0800

    3/7/2016 lab1

---
+++ lab1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int i;
+	cin>>i;
+
+	while(1)
+	{
+		if(i==1)
+		{
+			cout<<i<<' ';
+			break;
+		}
+
+		if(i%2==0)
+		{
+			cout<<i<<' ';
+			i=i/2;
+		}
+
+		else if(i%2==1)
+		{
+			cout<<i<<' ';
+			i=3*i+1;
+		}
+
+	}
+
+
+	return 0;
+}
